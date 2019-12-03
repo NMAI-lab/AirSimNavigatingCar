d7e0295596fdca8481d3409f6b72eea4461cacf8
James Baak <jamesbaak@protonmail.com>
2019-12-03 15:03:05 -0500

    Removed files and folders from generated make command to allow other users to build and run system

--- SAVE-master/catkin_ws/build/airsim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/james/catkin_ws/src/airsim
-# Build directory: C:/Users/james/catkin_ws/build/airsim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

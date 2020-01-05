59c9452ae2d14ab7837cda2c5f36389466057d91
James Baak <jamesbaak@protonmail.com>
2020-01-05 01:42:35 -0500

    Adding sensors package with speedometer example

---
+++ SAVE-master/catkin_ws/src/sensors/scripts/speedometer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import setup_path 
+import airsim
+
+import rospy
+from sensors.msg import Speed
+
+def speedometer():
+    pub = rospy.Publisher('speed', Speed, queue_size=1)
+    rospy.init_node('speedometer', anonymous=True)
+    rate = rospy.Rate(10) # 10 Hz
+
+    # connect to the AirSim simulator 
+    client = airsim.CarClient()
+    client.confirmConnection()
+
+    while not rospy.is_shutdown():
+        # Get the speed of the car
+        car_speed = client.getCarState().speed
+        rospy.loginfo(car_speed)
+        pub.publish(car_speed)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        speedometer()
+    except rospy.ROSInterruptException:
+        pass

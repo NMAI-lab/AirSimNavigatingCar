3e18de1b032a5f4f1cf47e91e4ed09d75ab03fd1
James Baak <jamesbaak@protonmail.com>
2020-02-23 02:15:08 -0500

    Lane assist improvements with lane margin correction

--- SAVE-master/catkin_ws/src/lka/scripts/lane_keep.py
+++ SAVE-master/catkin_ws/src/lka/scripts/lane_keep.py
@@ -10,11 +10,18 @@ from cv_bridge import CvBridge, CvBridgeError
 
 import lane_detect
 
+def margin_correction(left_line, right_line, height, width):
+    left_margin = (width/2) - ((height - left_line[1])/left_line[0])
+    right_margin = ((height - right_line[1])/right_line[0]) - (width/2)
+    return (right_margin - left_margin)/(width/2)
+
 def processImage(img):
     bridge  = CvBridge()
     steering = 0.0
     height = img.height
     width  = img.width
+    margin_steer = 0.0
+
     if img != None:
         try:
             cv_img = bridge.imgmsg_to_cv2(img, desired_encoding="passthrough")
@@ -22,16 +29,23 @@ def processImage(img):
             result = lane_detect.lane_detect(cv_img, (height, width))
             if result != None:
                 left_line, right_line = result
-                rospy.loginfo(result)
+                # rospy.loginfo(result)
 
-                if left_line != [] and right_line != []:
+                if left_line != [] and right_line != []: # There are both left and right lane lines
                     x_intersection = (left_line[1] - right_line[1])/(right_line[0] - left_line[0])
+                    margin_steer = margin_correction(left_line, right_line, height, width)
                 elif left_line == []: # Probably too close to the right side of the lane
                     x_intersection = ((height/2) - right_line[1])/(right_line[0] - 0)
                 elif right_line == []: # Probably too close to the left side of the lane
                     x_intersection = (left_line[1] - (height/2))/(0 - left_line[0])
 
-                steering = (x_intersection / (width / 2)) - 1
+                x_steering = (x_intersection / (width / 2)) - 1
+                if (margin_steer != 0):
+                    rospy.loginfo('margin correction: {}'.format(margin_steer))
+                    steering = (0.6 * x_steering) + (0.4 * margin_steer)
+                else:
+                    steering = x_steering
+
                 rospy.loginfo(steering)
                 steeringPub.publish(steering)
             else: # An error

bdfd7ae4c38c06cf901f61f295a0c62a2dda2be4
Patrick Gavigan <pgavigan@gmail.com>
2020-07-10 15:38:38 -0400

    Setting up messages for gps

--- catkin_ws/src/navigation/scripts/gpsSensor.py
+++ catkin_ws/src/navigation/scripts/gpsSensor.py
@@ -13,7 +13,7 @@ import airsim
 #import cv2
 
 import rospy
-from std_msgs.msg import Float64
+from navigation.msg import GPS
 
 def gpsSensor():
     # connect to the AirSim simulator
@@ -21,28 +21,30 @@ def gpsSensor():
     client.confirmConnection()
 
 
-    pub = rospy.Publisher('sensor/gps', Float64, queue_size=1)
+    pub = rospy.Publisher('sensor/gps', GPS, queue_size=1)
     rospy.init_node('gpsSensor', anonymous=True)
     rate = rospy.Rate(2) # 2 Hz
 
     while not rospy.is_shutdown():
 
         data = client.getGpsData()
+        message = GPS()
     
-        altitude = data.gnss.geo_point.altitude
-        latitude = data.gnss.geo_point.latitude
-        longitude = data.gnss.geo_point.longitude
-        position = (altitude,latitude,longitude)
+        # altitude = data.gnss.geo_point.altitude
+        message.latitude = data.gnss.geo_point.latitude
+        message.longitude = data.gnss.geo_point.longitude
+        message.bearing = 0
+        message.distance = 0
 
-        velocity = (data.gnss.velocity.x_val,
-                    data.gnss.velocity.y_val,
-                    data.gnss.velocity.z_val)
+        #velocity = (data.gnss.velocity.x_val,
+        #            data.gnss.velocity.y_val,
+        #            data.gnss.velocity.z_val)
 
         #print("(altitude, latitude, longitude): " + str(position))
         #print("velocity: " + str(velocity))
         
-        rospy.loginfo(latitude)
-        pub.publish(latitude)
+        rospy.loginfo(message)
+        pub.publish(message)
         rate.sleep()
 
 if __name__ == '__main__':

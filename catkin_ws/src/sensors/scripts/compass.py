781992591073de0f31b3d0bc42fab4a488eca627
Patrick Gavigan <pgavigan@gmail.com>
2020-11-20 11:56:56 -0500

    Getting the tun stuff working

--- catkin_ws/src/sensors/scripts/compass.py
+++ catkin_ws/src/sensors/scripts/compass.py
@@ -4,7 +4,8 @@ import airsim
 
 import rospy
 from std_msgs.msg import Float64
-from numpy import arctan, pi
+# from numpy import arctan, pi
+import math
 
 def compass():
     pub = rospy.Publisher('sensor/compass', Float64, queue_size=1)
@@ -22,23 +23,35 @@ def compass():
         
         rospy.loginfo(compassAngle)
         pub.publish(compassAngle)
+              
         rate.sleep()
 
 
+# THIS FUNCTION HAD THE POLARITY BACKWARDS!!!!!
 # https://cdn-shop.adafruit.com/datasheets/AN203_Compass_Heading_Using_Magnetometers.pdf
 # Direction (y>0) = 90 - [arcTAN(x/y)]*180/pi
 # Direction (y<0) = 270 - [arcTAN(x/y)]*180/pi
 # Direction (y=0, x<0) = 180.0
 # Direction (y=0, x>0) = 0.0  
-def getCompassAngle(x, y):
-    if y > 0:
-        return  90 - (arctan(x/y)) * 180 / pi   # Direction (y>0) = 90 - [arcTAN(x/y)]*180/pi
-    elif y < 0:
-        return 270 - (arctan(x/y)) * 180 / pi   # Direction (y<0) = 270 - [arcTAN(x/y)]*180/pi
-    elif ((y == 0) and (x < 0)):    # Direction (y=0, x<0) = 180.0
-        return 180.0
-    else:
-        return 0.0  # Direction (y=0, x>0) = 0.0
+#def getCompassAngle(x, y):
+#    if y > 0:
+#        return  90 - (arctan(x/y)) * 180 / pi   # Direction (y>0) = 90 - [arcTAN(x/y)]*180/pi
+#    elif y < 0:
+#        return 270 - (arctan(x/y)) * 180 / pi   # Direction (y<0) = 270 - [arcTAN(x/y)]*180/pi
+#    elif ((y == 0) and (x < 0)):    # Direction (y=0, x<0) = 180.0
+#        return 180.0
+#    else:
+#        return 0.0  # Direction (y=0, x>0) = 0.0
+    
+# THIS COMES FROM AVIONICS NAVIGATION SYSTEMS SECOND EDITION p. 443, (9.17)
+# Assumes that x = H_forward.
+# Avionics Navigation Systems, Second Edition
+# Editor(s): Myron Kayton Ph.D., P.E., Walter R. Fried M.S.,
+# First published:18 April 1997
+# Print ISBN:9780471547952 |Online ISBN:9780470172704 |DOI:10.1002/9780470172704
+# Copyright 1997 John Wiley & Sons, Inc.
+def getCompassAngle(x,y):
+    return math.degrees(math.atan(0 - (y/x)))
 
 if __name__ == '__main__':
     try:
--- catkin_ws/src/translators/scripts/turnTester.py
+++ catkin_ws/src/translators/scripts/turnTester.py
@@ -27,7 +27,9 @@ def updatePosition(data):
 
 def updateCompass(data):
     global currentBearing
-    currentBearing = data.data
+    declanation = 10.3881839942
+    compassReading = data.data
+    currentBearing = compassReading + declanation
 
 
 def setAngleRange180(angle):
@@ -40,21 +42,36 @@ def setAngleRange180(angle):
     return angle
 
 
-def getSteeringAngle(targetBearing, currentBearing):
-    
-    targetBearing = setAngleRange180(targetBearing)
-    currentBearing = setAngleRange180(currentBearing)
-    
-    diff = currentBearing - targetBearing
-    
-    
-    
-    # Return in radians
-    steeringAngle = math.radians(diff)   # See what happens (likely unstable)
-    return steeringAngle
+def calculateSteering(courseCorrection):
 
-    
-    
+    if abs(courseCorrection) < 20:
+        return courseCorrection/180
+    else:
+        if courseCorrection > 0:
+            return 1
+        else:
+            return -1
+
+
+# Use the control law defined here (good enough for now) - see (1):
+# https://www.researchgate.net/publication/307568375_Algorithm_of_autonomous_vehicle_steering_system_control_law_estimation_while_the_desired_trajectory_driving
+# deltaW =  wheel turn angle;
+# l = wheelbase;
+# R = turning radius
+# VCoG = velocity of vehicle center of gravity (speed of the car)
+# mCoG - assume this is the mass of the car
+# Assume that the front and rear tires do not slip (simplify the math)
+# deltaW = (l/R + mCoG)(VCoG^2 / R)
+#def calculateSteering(turnRadius, speed):
+#    # Unable to find values for the AirSim car. Use values for typical cars from online
+#    l = 3.025           # Assume Ford Explorer wheelbase of 3.025m
+#    R = turnRadius      # Assume Ford Explorer turning radius of 19.6" = 0.49784m
+#    mCoG = 2794.129     # Assume Ford Explorer mass of 6160lbs = 2794.129 kg
+#    VCoG = speed
+#
+#    deltaW = (l/R + mCoG) * ((VCoG * VCoG) / R)
+#    
+#    return math.radians(deltaW)
 
 # Main execution
 def directTo(steeringPub, speedPub, targetPosition):
@@ -62,26 +79,23 @@ def directTo(steeringPub, speedPub, targetPosition):
     global currentBearing
     global speed
     
-    delta = targetPosition.delta_to(currentPosition)
+    delta = currentPosition.delta_to(targetPosition)
     targetRange = delta.length[0]
     
-    steeringAngle = 0
     speedSetting = 6
-    steeringPub.publish(Float64(steeringAngle))
-    speedPub.publish(Float64(speedSetting))  
-    
+ 
     # While range to target > 5
     while targetRange > 10:
-        delta = targetPosition.delta_to(currentPosition)
+        delta = currentPosition.delta_to(targetPosition)
         targetRange = delta.length[0]
         targetBearing = delta.azimuth_deg[0]
-        steeringAngle = getSteeringAngle(targetBearing, currentBearing)        
-
-        rospy.loginfo("Steering angle (RAD): " + str(steeringAngle))
-        rospy.loginfo("Steering angle (DEG): " + str(math.degrees(steeringAngle)))
-
-        steeringAngle = 0
-        steeringPub.publish(Float64(steeringAngle))
+        
+        courseCorrection = targetBearing - currentBearing        
+        steering = calculateSteering(courseCorrection)
+        rospy.loginfo("course correction: " + str(courseCorrection))
+        rospy.loginfo("steering: " + str(steering))
+         
+        steeringPub.publish(Float64(steering))
         
         
         if speed <= 0:
@@ -91,7 +105,7 @@ def directTo(steeringPub, speedPub, targetPosition):
         
     rospy.loginfo("Arrived")
     # Stop the car
-    for i in range(1,20):
+    while speed > 0:
         steeringPub.publish(Float64(0))
         speedPub.publish(Float64(0))
         
@@ -108,20 +122,72 @@ def rosMain():
     rospy.Subscriber('sensor/gps', GPS, updatePosition)
     rospy.Subscriber('sensor/speed', Float64, updateSpeed)
 
-    sleep(5) 
+    global currentPosition
+    while currentPosition == (0,0):   # Wait for the first GPS update
+        sleep(1)
 
     # Test the method for directing the car somewhere using a GPS coordinate
+    post1 = wgs84.GeoPoint(latitude=47.641482370883864, longitude=-122.14036499180827, degrees = True)
+    post2 = wgs84.GeoPoint(latitude=47.6426242556, longitude=-122.140354517, degrees = True)
+    post3 = wgs84.GeoPoint(latitude=47.642632115856806, longitude=-122.13892325834075, degrees = True)
+    post4 = wgs84.GeoPoint(latitude=47.642634517703016, longitude=-122.14203898419318, degrees = True)
     
     targetPosition = wgs84.GeoPoint(latitude=47.6426242556, longitude=-122.140354517, degrees = True)
     directTo(steeringPub, speedPublisher, targetPosition)
 
     rospy.loginfo("Second leg")
-    targetPosition = wgs84.GeoPoint(latitude=47.642632115856806, longitude=-122.13892325834075, degrees = True)    
+    targetPosition = post3
     directTo(steeringPub, speedPublisher, targetPosition)
+
+def testMath():
+    compassReading = -10.3881839942
+    declanation = 10.3881839942
+    currentBearing = compassReading + declanation
+    
+    wgs84 = nv.FrameE(name='WGS84')
+    
+    post1 = wgs84.GeoPoint(latitude=47.641482370883864, longitude=-122.14036499180827, degrees = True)
+    post2 = wgs84.GeoPoint(latitude=47.6426242556, longitude=-122.140354517, degrees = True)
+    post3 = wgs84.GeoPoint(latitude=47.642632115856806, longitude=-122.13892325834075, degrees = True)
+    post4 = wgs84.GeoPoint(latitude=47.642634517703016, longitude=-122.14203898419318, degrees = True)
+        
+   
+    currentPosition = post1
+    targetPosition = post2
+
+    
+    
+    delta = currentPosition.delta_to(targetPosition)
+    targetRange = delta.length[0]
+    targetBearing = delta.azimuth_deg[0]
+    
+    courseCorrection = targetBearing - currentBearing
+    
+    print("Compass angle: " + str(currentBearing))
+    print("Target bearing: " + str(targetBearing))
+    print("Target range: " + str(targetRange))
+    
+    steering = calculateSteering(courseCorrection)
+    
+    print("Sample steering angle: " + str(steering))
+    
+    
+    # Straight ahead:
+    # -10.3881839942
+
+    # Need to turn to the left:
+    # 60
+
+    # Need to turn to the right:
+    # -60
 
 # Start things up
 if __name__ == '__main__':
     try:
         rosMain()
+    #testMath()
     except rospy.ROSInterruptException:
         pass
+
+
+

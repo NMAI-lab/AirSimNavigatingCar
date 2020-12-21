af4f5a8a8da4e49a79c15db6a45a69593554871f
Patrick Gavigan <pgavigan@gmail.com>
2020-12-21 11:35:08 -0500

    updated commands and perception translator

--- catkin_ws/src/translators/scripts/perceptionTranslatorNavAgent.py
+++ catkin_ws/src/translators/scripts/perceptionTranslatorNavAgent.py
@@ -3,30 +3,58 @@
 import rospy
 from std_msgs.msg import String
 from navigation.msg import GPS
+from std_msgs.msg import Float64
     
-# Global variables to remember current and previous qr codes
-position = (0,0)
+#position = (0,0)
+#compass = 0
+#speed = 0
 
 # Translate the line sensor data into a perception and publish
-def translatePerception(data, args):
+def translateGPS(data, args):
     # Extract the publisher and the message data
     (perceptionPublisher) = args
     
-    global position
+    #global position
     position = (data.latitude, data.longitude)
     
 
     # Publish the perception
-    gpsPerception = "gps({})".format(position)
+    gpsPerception = "gps{}".format(position)
     rospy.loginfo(gpsPerception)
-    #postPointPublisher.publish(postPoint)
     perceptionPublisher.publish(gpsPerception)
+
+def translateCompass(data, args):
+    # Extract the publisher and the message data
+    (perceptionPublisher) = args
+    
+    #global position
+    compass = data.data    
+
+    # Publish the perception
+    compassPerception = "compass({})".format(compass)
+    rospy.loginfo(compassPerception)
+    perceptionPublisher.publish(compassPerception)
+
+def translateSpeed(data, args):
+    # Extract the publisher and the message data
+    (perceptionPublisher) = args
+    
+    #global position
+    speed = data.data    
+
+    # Publish the perception
+    speedPerception = "speed({})".format(speed)
+    rospy.loginfo(speedPerception)
+    perceptionPublisher.publish(speedPerception)
+    
 
 # Initialize the node, setup the publisher and subscriber
 def rosMain():
     rospy.init_node('positionTranslator', anonymous=True)
     perceptionPublisher = rospy.Publisher('perceptions', String, queue_size=10)
-    rospy.Subscriber('sensor/gps', GPS, translatePerception, (perceptionPublisher))
+    rospy.Subscriber('sensor/gps', GPS, translateGPS, (perceptionPublisher))
+    rospy.Subscriber('sensor/compass', Float64, translateCompass, (perceptionPublisher))
+    rospy.Subscriber('sensor/speed', Float64, translateSpeed, (perceptionPublisher))
     rospy.spin()
 
 if __name__ == '__main__':

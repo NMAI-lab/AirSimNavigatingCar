4bf60ad6716af9b9ac35324a2a2b6281e2b223cc
Patrick Gavigan <pgavigan@gmail.com>
2020-12-21 10:28:37 -0500

    Finished and tested the car controller
    it works. Next, action translator

--- catkin_ws/src/controls/scripts/controller.py
+++ catkin_ws/src/controls/scripts/controller.py
@@ -1,44 +1,58 @@
 #!/usr/bin/env python
-from controls.msg import Control
+from std_msgs.msg import Float64
 from threading import Semaphore
 
 import rospy
 import airsim
 
-# Connect to the AirSim simulator and get controls object
-client = airsim.CarClient()
-client.confirmConnection()
-client.enableApiControl(True) # Enable API controls
-car_controls = airsim.CarControls()
-control_sem = Semaphore()
 
-def update_controls(controls):
-    global car_controls
-    rospy.loginfo(controls)
-    car_controls.brake = controls.brake
-    car_controls.throttle = controls.throttle
-    car_controls.steering = controls.steering
-    client.setCarControls(car_controls)
+def setup():
+    # Connect to the AirSim simulator and get controls object
+    global carControls, controlSem, simulator
+    simulator = airsim.CarClient()
+    simulator.confirmConnection()
+    simulator.enableApiControl(True) # Enable API controls
+    carControls = airsim.CarControls()
+    controlSem = Semaphore()
+
 
 def updateSteering(data):
+    rospy.loginfo(data)
+    global carControls, controlSem, simulator
+    controlSem.acquire()
+    carControls.steering = data.data
+    simulator.setCarControls(carControls)
+    controlSem.release()    
     
-    
+def updateBrake(data):
+    rospy.loginfo(data)
+    global carControls, controlSem, simulator
+    controlSem.acquire()
+    carControls.brake = data.data
+    simulator.setCarControls(carControls)
+    controlSem.release() 
+      
+def updateThrottle(data):
+    rospy.loginfo(data)
+    global carControls, controlSem, simulator
+    controlSem.acquire()
+    carControls.throttle = data.data
+    simulator.setCarControls(carControls) 
+    controlSem.release() 
 
 
 def controlListener():
     rospy.init_node('control', anonymous=True)
 
+    setup()
+
     # Subscribe to the different types of controls
-    rospy.Subscriber('action/steering', Control, updateSteering)
-    rospy.Subscriber('acc/throttle', Control, updateThrottle)
-    rospy.Subscriber('acc/brake', Control, updateBrake)
+    rospy.Subscriber('action/steering', Float64, updateSteering)
+    rospy.Subscriber('acc/throttle', Float64, updateThrottle)
+    rospy.Subscriber('acc/brake', Float64, updateBrake)
 
     # Repeat the listener
     rospy.spin()
-
-    # turn_layer = Layer(10, 'action', [('steering', Float64)])
-    # acc_layer = Layer(5, 'acc', [('throttle', Float64),('brake', Float64)])
-    # object_layer = Layer(10, 'object_avoid', [('brake', Float64)])
 
 
 if __name__ == '__main__':

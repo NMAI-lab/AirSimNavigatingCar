5bb1997dc023a7f92b1b9425f496caf8a8191ee9
Unknown <pgavigan@gmail.com>
2021-11-22 14:50:52 -0500

    Implementing the car state machine code
    Need to add the states

--- catkin_ws/src/controls/scripts/CarStateMachine.py
+++ catkin_ws/src/controls/scripts/CarStateMachine.py
@@ -6,6 +6,7 @@ Created on Thu Nov 18 10:18:24 2021
 """
 
 from stateMachine import StateMachine
+import nvframe as nv
 
 class CarStateMachine:
         
@@ -15,9 +16,18 @@ class CarStateMachine:
         self.nextWaypoint()
         self.destination()
         
+        self.declanation = 7.5
+        self.nearWaypointRange = 20
+        self.atWayPointRange = 7
+        self.obstacleRange = 5
+        
+        self.wgs84 = nv.FrameE(name='WGS84')
+        self.destination = self.wgs84.GeoPoint(latitude=6426242556, longitude=-122.140354517, degrees = True)
+        
     # TODO
     def loadStates(self):
         self.stateMachine.addStateTransition('Speed0', 'waypointFar', 'setSpeed(8)', 'SpeedFull')
+        
         
     def update(self, perception):
         (gps, lka, speed, compass, obstacle) = perception
@@ -27,43 +37,73 @@ class CarStateMachine:
         return action
     
     
-    # TODO
     # Need to generate a state machine trigger from the perception information
     def processPerception(self, gps, lka, obstacle):
+        positionTrigger = self.getPositionTrigger(gps)
+        lkaTrigger = self.getLkaTrigger(lka)
+        obstacleTrigger = self.getObstacleTrigger(obstacle)
+        return (positionTrigger, lkaTrigger, obstacleTrigger)
+    
+    
+    # waypoint: at, near, far
+    def getPositionTrigger(self, gps):
+        (curLat,curLon) = gps
+        current = self.wgs84.GeoPoint(latitude=curLat, longitude=curLon, degrees = True)
+        destinationRange = self.destination.delta_to(current).length
         
-        procesedPerception = 0
-    
+        position = ""
+        if destinationRange < self.atWayPointRange:
+            position = "at"
+        elif destinationRange < self.nearWaypointRange:
+            position = "near"
+        else:
+            position = "far"
         
-        # trigger outputs:
-        # waypoint: at, near, far
-        # lka: available, not available
-        # obstacle: near, far
-    
-        return procesedPerception
+        return position
     
     
+    # lka available: True, False
+    def getLkaTrigger(self, lka):
+        (_,_,_,c,d) = lka
+        if ((c != 0) or (d != 0)):
+            return True
+        else:
+            return False
     
-    # TODO
-    def getPositionTrigger(gps):
-        return 'meow'
+    # obstacle near: True, False
+    def getObstacleTrigger(self, obstacle):
+        if obstacle <= self.obstacleRange:
+            return True
+        else:
+            return False
         
-    # TODO
-    def getLkaTrigger(lka):
-        return 'meow'
-    
-    # TODO
-    def getObstacleTrigger(obstacle):
-        return 'meow'
-        
-    # TODO
     def processAction(self, action, lka, compass, gps):
-        processedAction = 0
+        if "lkaSteering" in action:
+            processedAction = "steering(" + str(self.getLkaSteeringAction(lka)) + ")"
+        elif "compassSteering" in action:
+            processedAction = "steering(" + str(self.getCompassSteering(gps, compass)) + ")"
+        else:
+            processedAction = action
+
         return processedAction
     
-    # TODO
-    def getCompassSteeringAction(compass,gps):
-        return 0
     
-    # TODO
-    def getLkaSteeringAction(lka):
-        return 0
+    def getCompassSteering(self, gps, compass):
+        (curLat,curLon) = gps
+        current = self.wgs84.GeoPoint(latitude=curLat, longitude=curLon, degrees = True)
+        destinationBearing = self.destination.delta_to(current).azimuth_deg[0]
+        courseCorrection = destinationBearing - (compass + self.declanation)
+        
+        if courseCorrection >= 20: 
+            steeringSetting = 1
+        elif courseCorrection <= -20: 
+            steeringSetting = -1
+        else: 
+            steeringSetting = courseCorrection/180
+            
+        return steeringSetting
+    
+
+    def getLkaSteeringAction(self, lka):
+        (steering,_,_,_,_) = lka
+        return steering
